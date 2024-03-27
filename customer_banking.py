cf057b8f0477b56f3d79c102f1a476d520eeb99a
awm66 <anthony.w.montgomery@gmail.com>
2024-03-27 15:43:38 -0600

    refactored variable names

--- customer_banking.py
+++ customer_banking.py
@@ -17,12 +17,12 @@ def main():
     interest_earned = 0
 
     # Call the create_savings_account function and pass the variables from the user.
-    updated_savings_balance, interest_earned = create_savings_account(savings_balance, savings_interest, savings_maturity)
-
+    updated_balance, interest_earned = create_savings_account(savings_balance, savings_interest, savings_maturity)
+    
     # Print out the interest earned and updated savings account balance with interest earned for the given months.
     # ADD YOUR CODE HERE
     print(f'You earned this amount of interest: ${interest_earned:.2f}')
-    print(f'Your updated balance is: ${updated_savings_balance:.2f}')
+    print(f'Your updated balance is: ${updated_balance:.2f}')
 
     # Prompt the user to set the CD balance, interest rate, and months for the CD account.
     # ADD YOUR CODE HERE
@@ -31,12 +31,12 @@ def main():
     cd_maturity = int(input('Enter the number of months for the CD: '))
 
     # Call the create_cd_account function and pass the variables from the user.
-    updated_cd_balance, interest_earned = create_cd_account(cd_balance, cd_interest, cd_maturity)
+    updated_balance, interest_earned = create_cd_account(cd_balance, cd_interest, cd_maturity)
 
     # Print out the interest earned and updated CD account balance with interest earned for the given months.
     # ADD YOUR CODE HERE
     print(f'You earned this amount of CD interest: ${interest_earned:.2f}')
-    print(f'Your updated CD balance is: ${updated_cd_balance:.2f}')
+    print(f'Your updated CD balance is: ${updated_balance:.2f}')
 
 if __name__ == "__main__":
     # Call the main function.
